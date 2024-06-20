220bea283ea117277efd99ab93cc9210617775f1
Ruchi Chaudhary <cruchi971@gmail.com>
2024-06-20 19:04:14 +0530

    Add files via upload
    When you click 'Next', the image magically transforms from phone size to desktop width using CSS transform property. 📱➡️💻#JavaScriptMagic

---
+++ image-slider/script.js
@@ -0,0 +1,13 @@
+console.log("Radhe-Radhe")
+
+
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+ next.addEventListener("click", ()=>{
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+ })
+ prev.addEventListener("click", ()=>{
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1])
+ })
